598917fae434a9537e3083f99ba9334122e64275
starship006 <33463067+starship006@users.noreply.github.com>
2021-09-07 22:32:45 -0500

    test commit on a fork
    woah. its a test commit on a fork since i dont have access to main repo.
    
    brandon give perms plz

---
+++ Assets/Extras/Scripts/TestScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// HELOOOOOOOOOO CODY WAS HERE DO NOT DELETE THIS IS RIGHTLY CLAIMED FILE 
+// IF YOU DELETE THIS I WILL DELETE YOU ! 
+
+
+
+
+
+
+
+
+
+
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
